a39d31d504dd424b128115c2ce9c8541f807fb1e
Osama Aboukoura <osama.aboukoura@kcl.ac.uk>
2018-09-17 20:45:08 +0100

    Fix styles

--- src/components/Pizza/OrderSummary/OrderSummary.js
+++ src/components/Pizza/OrderSummary/OrderSummary.js
@@ -84,8 +84,8 @@ class OrderSummary extends Component {
 
                 <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
                 <p>Continue to Checkout?</p>
-                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
                 <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
             </Aux>
         );
     }
